7fd25bcabefcd18df040856640e1834e1b6851f7
Tiago M Silva <tiagomms.15@gmail.com>
2024-04-10 09:07:13 +0100

    Calibration Start and Template scene ready but with environment rotation bug

--- Assets/01-Scripts/MoveXrOriginOnActions.cs
+++ Assets/01-Scripts/MoveXrOriginOnActions.cs
@@ -32,8 +32,8 @@ public class MoveXrOriginOnActions : MonoBehaviour
     public void MoveXrOrigin(GameAreaBoundaryProperties nextCageOrigin)
     {
         _characterController.enabled = false;
-        
-        transform.position = nextCageOrigin.transform.position;
+        float startLocationOffsetY = nextCageOrigin.StartLocation.localPosition.y;
+        transform.position = nextCageOrigin.transform.position + new Vector3(0, startLocationOffsetY - 0.01f, 0);
         // explainer: Environment is the parent and it may be rotated 90 degrees based on boundary size
         // so what matters is the fake cage local rotation
         transform.rotation = nextCageOrigin.transform.localRotation;
