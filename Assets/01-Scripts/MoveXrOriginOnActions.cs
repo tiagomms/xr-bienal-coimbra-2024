164424796ec3b36e0233d995ab1c786f5156da41
Tiago M Silva <tiagomms.15@gmail.com>
2024-04-12 14:53:17 +0100

    Undo enable/disable character controller on MoveXrOriginOnActions

--- Assets/01-Scripts/MoveXrOriginOnActions.cs
+++ Assets/01-Scripts/MoveXrOriginOnActions.cs
@@ -31,12 +31,12 @@ public class MoveXrOriginOnActions : MonoBehaviour
     
     public void MoveXrOrigin(GameAreaBoundaryProperties nextCageOrigin)
     {
-        _characterController.enabled = false;
+        //_characterController.enabled = false;
         float startLocationOffsetY = nextCageOrigin.StartLocation.localPosition.y;
         transform.position = nextCageOrigin.transform.position + new Vector3(0, startLocationOffsetY - 0.01f, 0);
         // explainer: Environment is the parent and it may be rotated 90 degrees based on boundary size
         // so what matters is the fake cage local rotation
         transform.rotation = nextCageOrigin.transform.localRotation;
-        _characterController.enabled = true;
+        //_characterController.enabled = true;
     }
 }
