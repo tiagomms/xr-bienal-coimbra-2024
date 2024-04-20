763a0f1b440432d13e41def2296aa1316e79ab79
Tiago M Silva <tiagomisilva@deloitte.pt>
2024-04-20 21:01:41 +0100

    Change script name, fix bug if already static

--- Assets/01-Scripts/99-Common/ForceObjectToBeStaticViaScript.cs
+++ Assets/01-Scripts/99-Common/ForceObjectToBeStaticViaScript.cs
@@ -2,9 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SetObjectStatic : MonoBehaviour
+public class ForceObjectToBeStaticViaScript : MonoBehaviour
 {
     [SerializeField] private bool setStaticAfterSceneSetup = true;
+    private bool isAlreadyStatic = false;
+
+    private void Start() {
+        isAlreadyStatic = gameObject.isStatic;
+    }
 
     private void OnEnable() 
     {
@@ -30,9 +35,13 @@ public class SetObjectStatic : MonoBehaviour
     // set object static
     public void ForceObjectToBeStatic()
     {
-        GameObject newThing = this.gameObject;
-        newThing.isStatic = true;
-        newThing.transform.parent = transform;
-        StaticBatchingUtility.Combine(gameObject);
+        if (!isAlreadyStatic)
+        {
+            GameObject newThing = this.gameObject;
+            newThing.isStatic = true;
+            newThing.transform.parent = transform;
+            StaticBatchingUtility.Combine(gameObject);
+            isAlreadyStatic = true;
+        }
     }
 }
